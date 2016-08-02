5120751a7891951ee1e041dcb8b548d8e346cf3e
mxxu <xumaoxing@gmail.com>
2016-08-02 22:24:20 +0800

    add routes

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
 import asyncio
 
+routes = {}
+
+class Request:
+    meth = 'GET'
+    path = '/'
+    heads = {}
+    body = ''
+
+class Response:
+    status_code = 200
+    content_type = "text/plain"
+    body = []
+
+def default_handler(request):
+    response = Response()
+    response.status_code = 404
+    return response
+
+def hello_handler(request):
+    response = Response()
+    response.body.append("hello: Mike!\r\n")
+    return response
+
+def register_route(meth, path, handler):
+    routes[(meth, path)] = handler
+
+def get_handler(meth, path):
+    return routes.get((meth, path), default_handler)
+
+register_route("GET", "/hello", hello_handler)
+
 async def handle(reader, writer):
     data = await reader.readline()
     if data == b"":
@@ -9,6 +40,11 @@ async def handle(reader, writer):
 
     data = data.decode()
     print(data)
+    meth, path, version = data.split()
+
+    request = Request()
+    request.meth = meth
+    request.path = path
     while True:
         data = await reader.readline()
         data = data.decode()
@@ -16,11 +52,22 @@ async def handle(reader, writer):
         if data == '\r\n':
             break
 
-    writer.write("HTTP/1.1 200 NA\r\n".encode())
-    writer.write("Content-Type: text/plain\r\n".encode())
+        key, value = data.rstrip().split(': ')
+        request.heads[key] = value
+
+    # writer.write("HTTP/1.1 200 NA\r\n".encode())
+    # writer.write("Content-Type: text/plain\r\n".encode())
+    # writer.write("\r\n".encode())
+    # writer.write("This is response body\r\n".encode())
+    # writer.write("This is response body line 2！！\r\n".encode())
+
+    handler = get_handler(meth, path)
+    response = handler(request)
+    writer.write("HTTP/1.1 {code} NA\r\n".format(code=response.status_code).encode())
+    writer.write("Content-Type: {tp}\r\n".format(tp=response.content_type).encode())
     writer.write("\r\n".encode())
-    writer.write("This is response body\r\n".encode())
-    writer.write("This is response body line 2！！\r\n".encode())
+    for s in response.body:
+        writer.write(s.encode())
     await writer.drain()
     writer.close()
 
